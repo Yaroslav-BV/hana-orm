02eaa398667d96c58623acabdbe8398507bc7c6f
Yaroslav-BV <yavabanote2@gmail.com>
2021-04-21 00:37:34 +0300

    Refactoring main interface. v1 - changed JSDoc

--- src/TableModel/index.js
+++ src/TableModel/index.js
@@ -5,12 +5,12 @@ const { selectTmp } = require('./queryTmp')
 const find = require('./find')
 const findOne = require('./findOne')
 
-/** @class TableModel - Класс для создания модели таблицы */
+/** @class TableModel Класс для создания модели таблицы */
 class TableModel {
   /**
-   * @param {string} table - Имя таблицы
-   * @param {object} param - Параметры таблицы
-   * @param {boolean} param.quotedName - Если true оборачивает имя таблицы в двойные кавычки
+   * @param {string} table Имя таблицы
+   * @param {Object} [param={}] Параметры таблицы
+   * @param {boolean} [param.quotedName] Если true оборачивает имя таблицы в двойные кавычки
    */
   constructor(table, param = {}) {
     const { quotedName } = param
@@ -19,25 +19,24 @@ class TableModel {
       throw Error('Invalid table name. table name is not String.')
     }
 
-    /** 
+    /**
      * @protected
-     * @property {string} table - Имя таблицы
+     * @property {string} table Имя таблицы
      */
     this._table = quotedName ? `"${table}"` : table
 
-    /** @method find - Выборка данных из таблицы */
+    /** @method find Выборка данных из таблицы */
     this.find = find
 
-    /** @method findOne - Выборка данных первого совпадения */
+    /** @method findOne Выборка данных по первому совпадению */
     this.findOne = findOne
   }
 
   /**
    * @protected
-   * @method _getSelectStm - Возвращает заполненный SELECT шаблон
-   * @param {object} p - Параметры для формирования стороки SELECT
-   * 
-   * @returns {string} - Возвращает сформированную строку SELECT
+   * @method _getSelectStm Возвращает заполненный SELECT шаблон
+   * @param {Object} p Параметры для формирования строки SELECT
+   * @returns {string} Возвращает сформированную строку SELECT
    */
   _getSelectStm(p) {
     for (const key in p) {
@@ -45,14 +44,7 @@ class TableModel {
         throw Error(`Value from prop "${key}" is not String.`)
     }
 
-    const selectStm = _.template(selectTmp)({
-      distinct: p.distinct,
-      top: p.top,
-      columns: p.columns,
-      table: p.table,
-      conditions: p.conditions,
-      orderBy: p.orderBy,
-    })
+    const selectStm = _.template(selectTmp)({ ...p })
 
     return selectStm
   }
