ab1859d94502b9f4011d1a22f57348e992752dcc
yermoart <yermoart@gmail.com>
2021-04-20 14:24:13 +0300

    Initial operators map [20.04.21: 14:24]

--- src/models/MagicWhere.js
+++ src/models/MagicWhere.js
@@ -8,65 +8,75 @@
  * 
  * @private
  */
- const Operators = 
- Object.freeze(
-   {
-     and: Symbol.for('and'),
-     or: Symbol.for('or'),
-     eq: Symbol.for('eq'),
-     ne: Symbol.for('ne')
-   }
- )
+const Operators = 
+  Object.freeze(
+    {
+      and: Symbol.for('and'),
+      or: Symbol.for('or'),
+      eq: Symbol.for('eq'),
+      ne: Symbol.for('ne')
+    }
+  )
+
+const OperatorsMap = 
+  Object.freeze(
+    {
+      [Operators.and]: ' AND ',
+      [Operators.or]: ' OR ',
+      [Operators.eq]: '=',
+      [Operators.ne]: '!='
+    }
+  )
 
 const operatorsSet = new Set(Object.values(Operators))
 
 /**
-* Получаем все не вложенные операторы
-* 
-* @param {Object} obj - Объект описывающий SQL условие
-* @returns {Array<symbol>} - Массив не вложенных операторов
-* 
-* @private
-*/
-const getOutOperators = (obj) => {
- return Object.getOwnPropertySymbols(obj).filter(s => operatorsSet.has(s));
+ * Получаем все не вложенные операторы
+ * 
+ * @param {Object} obj - Объект описывающий SQL условие
+ * @returns {Array<symbol>} - Массив не вложенных операторов
+ * 
+ * @private
+ */
+const _getOutOperators = (obj) => {
+  return Object.getOwnPropertySymbols(obj).filter(s => operatorsSet.has(s));
 }
 
 /**
-* Получаем все ключи включающие хоть какой-то оператор
-* 
-* @param {Object} obj - Объект описывающий SQL условие
-* @returns {Array<string|symbol>} All keys including operators
-* 
-* @private
-*/
-function getComplexKeys(obj) {
- return getOperators(obj).concat(Object.keys(obj));
+ * Получаем все ключи включающие хоть какой-то оператор
+ * 
+ * @param {Object} obj - Объект описывающий SQL условие
+ * @returns {Array<string|symbol>} Массив всех ключей объекта включающих оператор
+ * 
+ * @private
+ */
+const _getAllKeysIncludingOperator = (obj) => {
+  return _getOutOperators(obj).concat(Object.keys(obj));
 }
 
 function MagicWhere() {
- // console.log(getOutOperators(sql));
- // console.log(getComplexKeys(sql));
+  // console.log(_getOutOperators(example));
+  // console.log(_getAllKeysIncludingOperator(example));
 }
 
 /**
-* Пример SQL условия
-* 
-*/
-const sql = {
- a: {  
-   [Operators.eq]: 2
- },
- b: {
-   [Operators.or]: {
-     [Operators.eq]: 21,
-     [Operators.eq]: 11
-   }
- },
- [Operators.or]: {
-   c: 31,
-   d: 53
- }
+ * Пример SQL условия
+ * 
+ */
+const example = {
+  a: {  
+    [Operators.eq]: 2
+  },
+  b: {
+    [Operators.or]: {
+      [Operators.eq]: 21,
+      [Operators.eq]: 11
+    }
+  },
+  [Operators.or]: {
+    c: 31,
+    d: 53
+  }
 }
 
 module.exports = MagicWhere
