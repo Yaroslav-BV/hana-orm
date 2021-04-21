4901b5099ed7cc2d0487b7176e81c146fcf59eb8
yermoart <yermoart@gmail.com>
2021-04-21 20:40:53 +0300

    Add and/or grouping [21.04 20:40]

--- src/where-generator/where-generator.js
+++ src/where-generator/where-generator.js
@@ -23,11 +23,32 @@ function whereItems(where) {
     items.push(whereItem(prop, item))
   })
 
-  return items.join(' AND ')
+  return items.reverse().join(' AND ')
 }
 
 function whereItem(key, value) {
+
+  if(key === Op.and || key === Op.or) {
+    return whereGrouping(key, value)
+  }
+
   return Helpers.joinKeyValue(key, value, OpMap[Op.eq])
+}
+
+function whereGrouping(key, value) {
+  const op = key === Op.and ? OpMap[Op.and] : OpMap[Op.or]
+
+  if(Array.isArray(value)) {
+    value = value.map(item => {
+      return whereItems(item)
+    })
+
+    value = value.join(op);
+  } else {
+    throw new Error('Operator and/or supported only <array> for value')
+  }
+
+  return value
 }
 
 function getConditions(where) {
@@ -36,7 +57,10 @@ function getConditions(where) {
   return whereItems(
     {
       a: 1,
-      b: 2
+      [Op.or]: [
+        {b: 2},
+        {c: 3}
+      ]
     }
   ) || '1=1'
 }
